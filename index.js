ee923ee9bb7fd7e9d72bcb6bd2689e068bc0d846
Eloiza Sanrroman Melo <eloiza.melo@aluno.senai.br>
2026-05-08 14:44:17 -0300

    Site dia das maes

---
+++ index.js
@@ -0,0 +1,101 @@
+const carrossel = document.querySelector(".carrossel");
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const dots = document.querySelectorAll(".dot");
+
+let index = 0;
+
+function updateCarousel() {
+
+  carrossel.scrollTo({
+    left: index * carrossel.clientWidth,
+    behavior: "smooth"
+  });
+
+  dots.forEach(dot => dot.classList.remove("active"));
+
+  if (dots[index]) {
+    dots[index].classList.add("active");
+  }
+}
+
+nextBtn.addEventListener("click", () => {
+  index++;
+
+  if (index >= slides.length) {
+    index = 0;
+  }
+
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  index--;
+
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+
+  updateCarousel();
+});
+
+setInterval(() => {
+  index++;
+
+  if (index >= slides.length) {
+    index = 0;
+  }
+
+  updateCarousel();
+}, 5000);
+
+
+
+
+
+
+
+
+
+
+
+
+const envelope = document.getElementById("envelope");
+
+envelope.addEventListener("click", () => {
+  envelope.classList.toggle("open");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const musica = document.getElementById("musica");
+const musicBtn = document.getElementById("musicBtn");
+
+let tocando = false;
+
+musicBtn.addEventListener("click", () => {
+
+  if (!tocando) {
+    musica.play();
+    musicBtn.innerHTML = "❚❚ Pausar música";
+    tocando = true;
+
+  } else {
+    musica.pause();
+    musicBtn.innerHTML = "▶ Tocar música";
+    tocando = false;
+  }
+
+});
